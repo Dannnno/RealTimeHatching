d50ff85a304b92ecc56bcad60e32012753f58515
swarren <samuel.l.warren@gmail.com>
2016-05-08 12:04:40 -0700

    begin blending

--- ip2016/main.cpp
+++ ip2016/main.cpp
@@ -25,9 +25,10 @@ void checkGLError() {
     if (errCode != GL_NO_ERROR)
     {
         const GLubyte* errString = gluErrorString(errCode);
-        cerr << "OpenGL error: " << errString << endl;    } else {
-            cerr << "No error :)" << endl;
-        }
+        cerr << "OpenGL error: " << errString << endl;
+    } else {
+        cerr << "No error :)" << endl;
+    }
 }
 
 void normalize(GLfloat *a) {
@@ -111,8 +112,66 @@ void drawCone(double height, float baseRadius, int numStacks, int numSlices) {
     double stackHeight = height/numStacks;
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
+    
+    // draw the tip
+    float curStackY= height; //((2*radius*i)/numStacks)-radius;
+    float nextStackY = stackHeight*(numSlices-2);
+    float nextStackRadius= baseRadius - (((numSlices-1)*baseRadius)/(numStacks-1.));
+        for(int j=0; j<numSlices; j++) {
+            GLfloat v0 [3] = {static_cast<GLfloat>(0, curStackY, 0)};
+            GLfloat v1 [3] =  {static_cast<GLfloat>(nextStackRadius*cos(j*angleFraction)), nextStackY, static_cast<GLfloat>(nextStackRadius*sin(j*angleFraction))};
+            GLfloat v2 [3] = {static_cast<GLfloat>(nextStackRadius*cos((j+1)*angleFraction)), nextStackY, static_cast<GLfloat>(nextStackRadius*sin((j+1)*angleFraction))};
+            GLfloat m[3] = {
+                static_cast<GLfloat>(sqrt(pow(v0[0], 2) + pow(v0[2], 2))),
+                static_cast<GLfloat>(sqrt(pow(v1[0], 2) + pow(v1[2], 2))),
+                static_cast<GLfloat>(sqrt(pow(v2[0], 2) + pow(v2[2], 2)))
+            };
+            GLfloat n0 [3] = {0, 1, 0};            GLfloat n1 [3] = {
+                static_cast<GLfloat>((v1[0] / m[0]) * height / baseRadius),
+                static_cast<GLfloat>(baseRadius / height),
+                static_cast<GLfloat>((v1[2] / m[0]) * height / baseRadius),
+            };
+            GLfloat n2 [3] = {
+                static_cast<GLfloat>((v2[0] / m[0]) * height / baseRadius),
+                static_cast<GLfloat>(baseRadius / height),
+                static_cast<GLfloat>((v2[2] / m[0]) * height / baseRadius),
+            };
+            GLfloat lightVal[3] = {
+                getLightValue(v0, n0, lightPosition, NULL),
+                getLightValue(v1, n1, lightPosition, NULL),
+                getLightValue(v2, n2, lightPosition, NULL)
+            };
+            int texIndex[3] = {
+                getTextureIndex(lightVal[0]),
+                getTextureIndex(lightVal[1]),
+                getTextureIndex(lightVal[2])
+            };
+            //
+            //            glBlendFunc(GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA);
+            //            glEnable(GL_BLEND);
+            //            glEnable(GL_COLOR_MATERIAL);
+            
+            glColor4f(1.0f, 1.0f, 1.0f, .25f);
+            
+            for (int k = 0; k < 3; ++k) {
+                glEnable(GL_TEXTURE_2D);
+                glActiveTexture(textures[texIndex[k]]);
+                glBindTexture(GL_TEXTURE_2D, textures[texIndex[k]]);
+                glBegin(GL_TRIANGLES);
+                glNormal3fv(n0); glTexCoord2d((j+1)*angleFraction/(2*M_PI), curStackY/height);glVertex3fv(v0);
+                glNormal3fv(n1); glTexCoord2d(j*angleFraction/(2*M_PI), curStackY/height); glVertex3fv(v1);
+                glNormal3fv(n2); glTexCoord2d(j*angleFraction/(2*M_PI), nextStackY/height); glVertex3fv(v2);
+                glEnd();
+                glDisable(GL_TEXTURE_2D);
+            }
+            
+            //            glDisable(GL_BLEND);
+            //            glDisable(GL_COLOR_MATERIAL);
+            
+        }
 
-    for(int i=0; i<numStacks-1; i++){
+
+    for(int i=1; i<numStacks-1; i++){
         float curStackY= stackHeight*i; //((2*radius*i)/numStacks)-radius;
         float curStackRadius = baseRadius - ((i*baseRadius)/(numStacks-1.));
         float nextStackY = stackHeight*(i+1);
@@ -295,6 +354,7 @@ void drawSphereWithQuads(float radius, int numSlices, int numStacks){
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     //draw sphere with center at origin from bottom up
+
     for(int i=0; i<numStacks; i++){
         float curStackY= ((2*radius*i)/numStacks)-radius;
         float curStackRadius = sqrt(pow(radius, 2)-pow(curStackY, 2));
@@ -316,17 +376,19 @@ void drawSphereWithQuads(float radius, int numSlices, int numStacks){
                 getTextureIndex(lightVal[0]),
                 getTextureIndex(lightVal[1]),
                 getTextureIndex(lightVal[2]),
-                getTextureIndex(lightVal[4])
+                getTextureIndex(lightVal[3])
             };
             
-            glBlendFunc(GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA);
+
             glEnable(GL_BLEND);
-            glEnable(GL_COLOR_MATERIAL);
+            glEnable(GL_TEXTURE_2D);
+            glBlendFunc(GL_DST_ALPHA, GL_ONE);
+            //glEnable(GL_COLOR_MATERIAL);
             
-            glColor4f(1.0f, 1.0f, 1.0f, .25f);
+//            glColor4f(1.0f, 1.0f, 1.0f, .25f);
             
             for (int k = 0; k < 4; ++k) {
-                glEnable(GL_TEXTURE_2D);
+
                 glActiveTexture(textures[texIndex[k]]);
                 glBindTexture(GL_TEXTURE_2D, textures[texIndex[k]]);
                 glBegin(GL_QUADS);
@@ -335,9 +397,9 @@ void drawSphereWithQuads(float radius, int numSlices, int numStacks){
                 glNormal3fv(v2); setSphereTexCoord(v2, radius); glVertex3fv(v2);
                 glNormal3fv(v3); setSphereTexCoord(v3, radius); glVertex3fv(v3);
                 glEnd();
-                glDisable(GL_TEXTURE_2D);
+       
             }
-            
+            glDisable(GL_TEXTURE_2D);
             glDisable(GL_BLEND);
             glDisable(GL_COLOR_MATERIAL);
 
@@ -412,7 +474,11 @@ void loadTAM(){
     for (int tone = 0; tone < NUMBER_OF_TONES; ++tone) {
         for (int res = 0; res < NUMBER_OF_RESOLUTIONS; ++res) {
             char filename[100];
-            sprintf(filename, "/Users/swarren/Downloads/ip2016skeleton/TAM_tone%d_resolution%d.bmp", tone, res);
+            //uncomment line below and comment line after to have correct path on Sam's comp, line below
+            //is correct file path for github code 
+//            sprintf(filename, "/Users/swarren/Downloads/ip2016skeleton/TAM_tone%d_resolution%d.bmp", tone, res);
+            sprintf(filename, "/Users/swarren/Downloads/ip2016skeleton/ip2016/TAMimages/TAM_tone%d_resolution%d.bmp", tone, res);
+
             tamNames[tone][res] = std::string(filename);
         }
     }
@@ -421,9 +487,9 @@ void loadTAM(){
     for (int tone = 0; tone < NUMBER_OF_TONES; ++tone) {
         glBindTexture(GL_TEXTURE_2D, textures[tone]);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 8);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, NUMBER_OF_RESOLUTIONS-1);
         glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
 
         for (int resolution = 0; resolution < tamNames[tone].size(); ++resolution) {
@@ -483,8 +549,8 @@ void init()
     glLoadIdentity();
     
     // enable light0 and lighting
-    glEnable(GL_LIGHT0);
-    glEnable(GL_LIGHTING);
+//    glEnable(GL_LIGHT0);
+//    glEnable(GL_LIGHTING);
     //glEnable(GL_COLOR_MATERIAL);
     
     // set color of light0
@@ -493,16 +559,16 @@ void init()
     glLightfv(GL_LIGHT0, GL_SPECULAR, white);	// set specular light color
     
     // initialize background color to black
-    glClearColor(0,0,0,0);
+    glClearColor(0,0,0,.25);
     
     // enable depth buffering
-    glEnable(GL_DEPTH_TEST);
+    glDisable(GL_DEPTH_TEST);
 }
 
 void mouse(int button, int state, int x, int y)
 {
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-        cout << "Left click with cursor at" << x << " " << y << endl;
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN);
+//        cout << "Left click with cursor at" << x << " " << y << endl;
 }
 
 void motion(int x, int y)
@@ -539,39 +605,74 @@ void reshape(int width, int height)
     
 }
 
-
-
+void draw_quad()
+{
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glEnable(GL_BLEND);
+//    glTexEnvf(, <#GLenum pname#>, )
+    glBlendFunc(GL_DST_ALPHA, GL_ONE);
+    glEnable(GL_TEXTURE_2D);
+//    glColor3f(1,1,1);
+    for (int k = 0; k < 4; ++k) {
+//        if (k==0) {
+//          
+//            glColor3f(1,0,0);
+//        }
+//        if (k==1)
+//        {
+//            glColor3f(0,1,0);
+//        }
+//        if (k==2) {
+//            glColor3f(0, 0, 1);
+//        }
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+        
+//        printf("here\n");
+//        checkGLError();
+//        printf("there\n");
+    glNormal3f(0, 0, 1);
+    glBegin(GL_QUADS);
+    glTexCoord2i(0, 0); glVertex3f(0, 0, 0);
+    glTexCoord2i(1, 0); glVertex3f(1, 0, 0);
+    glTexCoord2i(1, 1); glVertex3f(1, 1, 0);
+    glTexCoord2i(0, 1); glVertex3f(0, 1, 0);
+    glEnd();
+//    }
+//    glBegin(GL_QUADS);
+//    glVertex3f(0, 0, 0);
+//    glVertex3f(1, 0, 0);
+//    glVertex3f(1, 1, 0);
+//    glVertex3f(0, 1, 0);
+//    glEnd();
+}
+    glDisable(GL_TEXTURE_2D);
+    glDisable(GL_BLEND);
+    glDisable(GL_CULL_FACE);
+}
 
 
 void display()
 {
-    
-    GLfloat white[] = {1,1,1,0};	    // white
-    GLfloat red[] = {1,0,0,0};              // red
-    GLfloat green[] = {0,1,0,0};             // green
-    GLfloat purple[] = {1,0,1,0};	    // purple
-    
     // clear buffers
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
     // initialize modelview matrix
-    glMatrixMode(GL_MODELVIEW_MATRIX);
+    glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     
     // set viewpoint position/orientation
     gluLookAt(10*sin(phi*3.14/180.0),5,10*cos(phi*3.14/180.0),0,0,0,0,1,0);
     // position of light0
     glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
-    
-    glNormal3f(0,0,1);
-    glNormal3f(1,0,0);
-    glColor3d(1, 1, 1);
 
     
-   // drawCylinder(20, 1, 1);
+//    drawCylinder(20, 1, 1);
     drawSphereWithQuads(1, 50, 50);
 //    drawCone(2, 1, 15, 15);
 //    drawCubeWithQuads(2, 50);
+//    draw_quad();
     
     // swap buffers
     glutSwapBuffers();
